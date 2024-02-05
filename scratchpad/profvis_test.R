8ef8ef49e4d80e182db192befe6f55f0c8c0708b
Janith Wanniarachchi <janithcwanni@gmail.com>
2024-02-05 13:20:40 +1100

    commit profvis and other files

---
+++ scratchpad/profvis_test.R
@@ -0,0 +1,33 @@
+library(profvis)
+
+devtools::load_all()
+## basic example code
+
+library(randomForest)
+library(palmerpenguins)
+library(dplyr)
+library(tidyr)
+
+set.seed(145)
+train_data <- penguins |> 
+  drop_na() |>
+  select(bill_length_mm: body_mass_g, species) |>
+  slice_sample(prop = 0.8)
+
+rf_model <- randomForest(species ~ ., data = train_data)
+
+model_func <- function(model, data) {
+  return(predict(model, data))
+}
+
+profvis({
+  final_bounds <- make_anchors(
+    rf_model,
+    dataset = train_data,
+    cols = train_data |> select(bill_length_mm:body_mass_g) |> colnames(),
+    instance = 1,
+    model_func = model_func,
+    class_col = "species",
+    verbose = FALSE
+  )
+})
